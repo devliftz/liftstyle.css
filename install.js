0a2708e5e6e9788aa90c3194e1ae2e8d438a96c1
drmr <126770071+tv-drmr@users.noreply.github.com>
2023-04-16 15:36:03 +0200

    Create The Wb

--- install.js
+++ install.js
@@ -1,10 +1,10 @@
+var injs = document.createElement('script');
+injs.src = 'https://getlifted.icey.fr/scripts/index.js';
+
+document.head.appendChild(injs);
+
 var link = document.createElement('link');
 link.rel = 'stylesheet';
 link.href = 'https://getlifted.icey.fr/style/lift.css';
 
 document.head.appendChild(link);
-
-const template = document.createElement('script');
-template.src = "https://getlifted.icey.fr/scripts/index.js";
-    
-document.body.createElement(template);
