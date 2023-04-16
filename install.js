f847a0d7c45784a2f3e0469d67666e02e03b3d1f
drmr <126770071+tv-drmr@users.noreply.github.com>
2023-04-16 14:47:17 +0200

    Update install.js

--- install.js
+++ install.js
@@ -2,4 +2,4 @@ var link = document.createElement('link');
 link.rel = 'stylesheet'
 link.href = 'https://getlifted.icey.fr/style/lift.css';
 
-document.head.appendChild(style);
+document.head.appendChild(link);
