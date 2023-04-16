3569567d0905aa5fa441cadf8a2be7f8723e5191
drmr <126770071+tv-drmr@users.noreply.github.com>
2023-04-16 15:11:04 +0200

    Update install.js

--- install.js
+++ install.js
@@ -1,9 +1,9 @@
 var link = document.createElement('link');
-link.rel = 'stylesheet'
+link.rel = 'stylesheet';
 link.href = 'https://getlifted.icey.fr/style/lift.css';
 
 var script = document.createElement('script');
-script.src = 'https://getlifted.icey.fr/scripts/index.js'
+script.src = 'https://getlifted.icey.fr/scripts/index.js';
 
 document.head.appendChild(link);
 document.body.appendChild(script);
